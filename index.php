28b3fdc07d09fadfc20beffb5f44dff4b34332a1
Atom Yah <atom@yah>
2017-11-28 15:44:08 +0900

    p048 modify

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 require_once __DIR__ .'/vendor/autoload.php';
 
-$inputString = file_get_contents('php://input');
-error_log($inputString);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+
+$bot = new \LINE\LINEBot($httpClinet, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+$signiture = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+$event = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
+foreach ($events as $event) {
+  $bot->replyText($event->getReplyToken(), 'TextMessage');
+}
 
 ?>
