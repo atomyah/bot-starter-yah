bbc4463919190cd0498509486abdcbedf6a41168
Atom Yah <atom@yah>
2017-11-28 16:50:40 +0900

    P50 modify

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ foreach ($events as $event) {
 function replyTextMessage($bot, $replyToken, $text) {
   $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
 
-  if (!$response->isSuccessfu()) {
+  if (!$response->isSucceeded()) {
     error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
   }
   
