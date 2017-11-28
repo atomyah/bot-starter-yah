d8a82edeb0a1df5844baaf7587d3a0b55d3b0026
Atom Yah <atom@yah>
2017-11-28 15:51:32 +0900

    p048 modify

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 
 $signiture = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-$event = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
 foreach ($events as $event) {
   $bot->replyText($event->getReplyToken(), 'TextMessage');
