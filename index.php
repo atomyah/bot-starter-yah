ff7a603bc25bea069d2dc3aa08ec1b3cd8b648df
Atom Yah <atom@yah>
2017-11-29 12:53:43 +0900

    P53 modify

--- index.php
+++ index.php
@@ -11,7 +11,10 @@ $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
 foreach ($events as $event) {
 //  replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
-  replyImageMessage($bot, $event->getReplyToken(), 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg', 'https://' . $_SERVER['HTTP_HOST'] . 'imgs/preview.jpg');
+//  replyImageMessage($bot, $event->getReplyToken(), 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg', 'https://' . $_SERVER['HTTP_HOST'] . 'imgs/preview.jpg');
+  replyLocationMessage($bot, $event->getReplyToken(), 'LINE', '東京都渋谷区渋谷2-21-1 ヒカリエ27階', '35.659025', '139.703473');
+  
+  
 }
 
 function replyTextMessage($bot, $replyToken, $text) {
@@ -29,6 +32,14 @@ function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUr
   if (!$response->isSucceeded()) {
     error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
   }
+}
+  
+  function replyLocationMessage($bot, $replyToken, $title, $address, $lat, $lon) {
+    $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title, $address, $lat, $lon));
+  
+  if (!$response->isSucceeded()) {
+    error_log('Failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }  
 }
 
 ?>
